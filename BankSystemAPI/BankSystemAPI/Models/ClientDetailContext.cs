320ba9b12457a5df65451bf0371254163543c23f
Nevena <milacicnevena@gmail.com>
2019-11-16 14:17:19 +0100

    Create database

---
+++ BankSystemAPI/BankSystemAPI/Models/ClientDetailContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankSystemAPI.Models
+{
+    public class ClientDetailContext:DbContext
+    {
+        public ClientDetailContext(DbContextOptions<ClientDetailContext> options):base (options)
+        {
+
+        }
+        public DbSet<ClientDetail> ClientDetail { get; set; }
+
+    }
+}
--- BankSystemAPI/BankSystemAPI/Startup.cs
+++ BankSystemAPI/BankSystemAPI/Startup.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+  
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,10 +8,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using BankSystemAPI.Models;
 
 namespace BankSystemAPI
 {
@@ -25,7 +30,15 @@ namespace BankSystemAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options => {
+                    var resolver = options.SerializerSettings.ContractResolver;
+                    if (resolver != null)
+                        (resolver as DefaultContractResolver).NamingStrategy = null;
+                });
+            services.AddDbContext<BankSystemAPI.Models.ClientDetailContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,8 +54,14 @@ namespace BankSystemAPI
                 app.UseHsts();
             }
 
+            app.UseCors(options =>
+           options.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader());
+
             app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
 }
+
